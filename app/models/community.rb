632c5ad411d25fef73c5e64e6053644bff276baf
Oussama Issaoui <oussama@appid.tn>
2015-08-15 17:16:27 +0100

    fixing paperclip

--- app/models/community.rb
+++ app/models/community.rb
@@ -181,7 +181,6 @@ class Community < ActiveRecord::Base
                                                       "image/gif",
                                                       "image/pjpeg",
                                                       "image/x-png"]
-  process_in_background :logo
 
   has_attached_file :wide_logo,
                     :styles => {
@@ -202,7 +201,6 @@ class Community < ActiveRecord::Base
                                                       "image/gif",
                                                       "image/pjpeg",
                                                       "image/x-png"]
-  process_in_background :wide_logo
 
   has_attached_file :cover_photo,
                     :styles => {
@@ -219,7 +217,6 @@ class Community < ActiveRecord::Base
                                                       "image/gif",
                                                       "image/pjpeg",
                                                       "image/x-png"]
-  process_in_background :cover_photo
 
   has_attached_file :small_cover_photo,
                     :styles => {
@@ -236,7 +233,6 @@ class Community < ActiveRecord::Base
                                                       "image/gif",
                                                       "image/pjpeg",
                                                       "image/x-png"]
-  process_in_background :small_cover_photo
 
   has_attached_file :favicon,
                     :styles => {
@@ -255,6 +251,11 @@ class Community < ActiveRecord::Base
                                                       "image/gif",
                                                       "image/x-icon",
                                                       "image/vnd.microsoft.icon"]
+
+  process_in_background :logo
+  process_in_background :wide_logo
+  process_in_background :cover_photo
+  process_in_background :small_cover_photo
   process_in_background :favicon
 
   validates_format_of :twitter_handle, with: /\A[A-Za-z0-9_]{1,15}\z/, allow_nil: true
