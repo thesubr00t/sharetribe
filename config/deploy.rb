f7b32a11b16f3a9f7bb70c1a2b6ac018b0aa513e
Oussama Issaoui <oussama@appid.tn>
2015-08-16 17:10:52 +0100

    config.yml > share

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:thesubr00t/sharetribe.git'
 
 set :deploy_to, '/home/deploy/eurobizz'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/config.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :delayed_job_server_role, :worker
